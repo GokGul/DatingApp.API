58978159391c470cc4c9fa625e35ce7996d0f357
gokguu <gokhan_g32@yahoo.com>
2018-05-26 14:24:41 +0200

    Initial commit

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API
 {
@@ -23,6 +25,7 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
